86ac08491f156a714dd1822b3d413946158ab8da
Georgie C <g@georgiechallis.com>
2021-07-24 23:12:29 +0100

    Add nose detect

--- face-landmarks-detection.py
+++ face-landmarks-detection.py
@@ -1,8 +1,8 @@
 '''
 https://pysource.com/2019/03/12/face-landmarks-detection-opencv-with-python/
+https://learnopencv.com/head-pose-estimation-using-opencv-and-dlib/
 
 '''
-
 
 
 import cv2
@@ -15,6 +15,12 @@ detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 
 numLandmarks = 68
+chin = 9
+nose = 34
+left_eye = 37
+right_eye = 46
+left_mouth = 49
+right_mouth = 55
 
 def displayLandmarks(frame, gray, face):
     landmarks = predictor(gray, face)
@@ -23,6 +29,12 @@ def displayLandmarks(frame, gray, face):
         x = landmarks.part(n).x
         y = landmarks.part(n).y
         cv2.circle(frame, (x, y), 4, (255, 0, 0), -1)
+
+def displayNose(frame, gray, face):
+    landmarks = predictor(gray, face)
+    x = landmarks.part(nose-1).x
+    y = landmarks.part(nose-1).y
+    cv2.circle(frame, (x, y), 4, (255, 0, 0), -1)
 
 def displayFaceTrack():
     cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 3)
@@ -38,8 +50,9 @@ while True:
         x2 = face.right()
         y2 = face.bottom()
 
-        displayLandmarks(frame, gray, face)
-        displayFaceTrack()
+        #displayLandmarks(frame, gray, face)
+        #displayFaceTrack()
+        displayNose(frame,gray,face)
 
 
     cv2.imshow("Frame", frame)
